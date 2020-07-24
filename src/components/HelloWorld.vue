6260c80a4074b5b29057e2707956834fdf3aa0f4
happyt <ian.happytuesdays@gmail.com>
2020-07-24 18:37:47 +0100

    log notify

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="hello">
     <h1>Austria Map</h1>
-    <p>
-      Please click a state to view state properties
-    </p>
+    <p>Please click a state to view state properties</p>
     <div>
       <austria-map v-on:map-clicked="onMapClick"></austria-map>
     </div>
@@ -11,23 +9,24 @@
 </template>
 
 <script>
-import AustriaMap from './AustriaMap.vue'
+import AustriaMap from "./AustriaMap.vue";
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   components: {
-    AustriaMap
+    AustriaMap,
   },
   props: {
-    msg: String
+    msg: String,
   },
   methods: {
-    onMapClick: function(attr){
+    onMapClick: function (attr) {
+      console.log("notify");
       this.$notify({
-        group: 'map',
-        title: 'State clicked',
-        text: `You clicked on state with id: ${attr.mapId} and title: ${attr.title}`
+        group: "map",
+        title: "State clicked",
+        text: `You clicked on state with id: ${attr.mapId} and title: ${attr.title}`,
       });
-    }
-  }
-}
+    },
+  },
+};
 </script>
